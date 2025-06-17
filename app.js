91c72789de682db834fb351b94a3c3e8556bbdfc
wesleyHW <freire.wesley@escola.pr.gov.br>
2025-06-17 14:28:02 +0000

    ok

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
-    let cartao = document.createElement ('article')
+    let cartao = document.createElement('article')
     cartao.className = 'cartao'
-    cartao.innerHTML =`
+    cartao.innerHTML = `
+        <article class="cartao">
         <div class="cartao__conteudo">
             <h3>Programação</h3>
             <div class="cartao__conteudo">
@@ -14,6 +15,17 @@ function criaCartao(categoria, pergunta, resposta) {
                 <div class="cartao__conteudo__resposta">
                     <p>${resposta}<p/>
                 </div>
+                </div>
+                </article>
             `
-            container.appendChild(cartao)
+
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active',respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+    container.appendChild(cartao)
 }
